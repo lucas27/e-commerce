2158b178147121f22fe6ebb281772b14f619ef50
lucas27 <lucasbrasileiro27@gmail.com>
2026-06-30 20:04:04 -0300

    feat: add exists

--- src/main/java/com/projeto/e_commerce/order/repository/OrderRepository.java
+++ src/main/java/com/projeto/e_commerce/order/repository/OrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projeto.e_commerce.order.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Integer>{
-    
+    boolean existsByUser_id(Integer userId);
 }
