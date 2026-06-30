ed7fed7b0fb3a0100b48f9e96182874ba9ed6e4d
lucas27 <lucasbrasileiro27@gmail.com>
2026-06-30 20:02:43 -0300

    feat: add query

--- src/main/java/com/projeto/e_commerce/auth/repository/UserRepository.java
+++ src/main/java/com/projeto/e_commerce/auth/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.projeto.e_commerce.auth.entity.User;
 
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // devido ao jpa e oauth2, deve utilizar o user, e não o userDetails
     Optional<User> findUserByEmail(String username);
     boolean existsByEmail(String email);
+
+    @Query("SELECT u.id FROM User u WHERE u.email = :email")
+    Integer findOnlyIdByEmail(@Param("email") String username);
 } 
