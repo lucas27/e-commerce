7444580902c7ed5a0e9f3c71fbc4fc5effff397e
lucas27 <lucasbrasileiro27@gmail.com>
2026-06-30 20:04:37 -0300

    feat: add new exceptions

--- src/main/java/com/projeto/e_commerce/handler/ExceptionsHadler.java
+++ src/main/java/com/projeto/e_commerce/handler/ExceptionsHadler.java
@@ -2,6 +2,7 @@ package com.projeto.e_commerce.handler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.projeto.e_commerce.exception.EntityExistException;
 import com.projeto.e_commerce.exception.IdNotFoundException;
+import com.projeto.e_commerce.exception.QuantityExceededException;
 import com.projeto.e_commerce.exception.dto.ExceptionMessageDto;
 
 @RestControllerAdvice
@@ -29,6 +31,26 @@ public class ExceptionsHadler {
         ExceptionMessageDto dto = new ExceptionMessageDto();
 
         dto.setExceptionCode(HttpStatus.NOT_FOUND.value());
+        dto.setMessage(e.getMessage());
+        dto.setExceptionDateTime(LocalDateTime.now());
+        return ResponseEntity.status(dto.getExceptionCode()).body(dto);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionMessageDto> Exception(DataIntegrityViolationException e) {
+        ExceptionMessageDto dto = new ExceptionMessageDto();
+
+        dto.setExceptionCode(HttpStatus.CONFLICT.value());
+        dto.setMessage(e.getMessage());
+        dto.setExceptionDateTime(LocalDateTime.now());
+        return ResponseEntity.status(dto.getExceptionCode()).body(dto);
+    }
+
+    @ExceptionHandler(QuantityExceededException.class)
+    public ResponseEntity<ExceptionMessageDto> Exception(QuantityExceededException e) {
+        ExceptionMessageDto dto = new ExceptionMessageDto();
+
+        dto.setExceptionCode(HttpStatus.CONFLICT.value());
         dto.setMessage(e.getMessage());
         dto.setExceptionDateTime(LocalDateTime.now());
         return ResponseEntity.status(dto.getExceptionCode()).body(dto);
