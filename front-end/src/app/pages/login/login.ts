8abedcd5b83417e551a2d2ed1cee0e7e81a36588
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-07 17:07:26 -0300

    feat: add login logic

--- front-end/src/app/pages/login/login.ts
+++ front-end/src/app/pages/login/login.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 
+// ver static, readonly
+interface User {
+  login: string;
+  password: string;
+}
+// interface tem que está acima do @component
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+
+export class Login {
+  accountLogin: User | null = null;
+  
+  getValue(email:string, password:string) {
+    this.accountLogin = {
+      login: email,
+      password: password
+    }
+    console.log(this.accountLogin);
+  };
+}
