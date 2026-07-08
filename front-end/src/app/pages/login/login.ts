f285c5ed59472ad89e46a507bbabf642ee11efca
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-08 20:47:39 -0300

    feat: add login features

--- front-end/src/app/pages/login/login.ts
+++ front-end/src/app/pages/login/login.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LucideCircleUser, LucideFileText, LucideLock, LucideLockKeyholeOpen, LucideUser } from '@lucide/angular';
+import { RouterLink } from "@angular/router";
+import { NgIf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 // ver static, readonly
 interface User {
@@ -8,16 +11,29 @@ interface User {
 // interface tem que está acima do @component
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LucideLock, LucideUser, LucideCircleUser, LucideLockKeyholeOpen, RouterLink],
+  // template: '<svg lucideLock></svg>',
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 
 
 export class Login {
-  accountLogin: User | null = null;
-  
+  private accountLogin: User | null = null;
+  protected viewPassword: boolean = false;
+
+  changeVisibility(){
+    this.viewPassword = !this.viewPassword;
+    console.log(this.viewPassword);
+  };
+
   getValue(email:string, password:string) {
+    if(!email.includes("@gmail.com") || email.trim() === "") {
+      throw new Error("digite um email válido");
+    } else if(password.length < 8 || password.trim() === "") {
+      throw new Error("A senha tem que ter mais de 8 caracteres");
+    }
+
     this.accountLogin = {
       login: email,
       password: password
