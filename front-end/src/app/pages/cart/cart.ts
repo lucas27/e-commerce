f504ca548d6b6fe650727983e10d4d4d690ff89f
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-12 13:23:28 -0300

    feat: update angular

--- front-end/src/app/pages/cart/cart.ts
+++ front-end/src/app/pages/cart/cart.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   imports: [],
   templateUrl: './cart.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './cart.css',
 })
 export class Cart {}
--- front-end/src/app/pages/login/component/validation-message/validation-message.ts
+++ front-end/src/app/pages/login/component/validation-message/validation-message.ts
@@ -1,10 +1,11 @@
-import { Component, inject, input, Input } from '@angular/core';
+import { Component, inject, input, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Login } from '../../login';
 
 @Component({
   selector: 'app-validation-message',
+  changeDetection: ChangeDetectionStrategy.Eager,
   template: '<p class="text-white pl-18">{{message}}</p>',
 })
 export class ValidationMessage {
-  @Input() message: string = "";
+  @Input() message: string = '';
 }
