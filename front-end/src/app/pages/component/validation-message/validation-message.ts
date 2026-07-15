00c26752d7e91ec2045a26293d6843a4d907e0cb
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-15 19:12:14 -0300

    fix: change message variable

--- front-end/src/app/pages/component/validation-message/validation-message.ts
+++ front-end/src/app/pages/component/validation-message/validation-message.ts
@@ -1,10 +1,10 @@
-import { Component, inject, input, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, inject, input, Input, ChangeDetectionStrategy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-validation-message',
   changeDetection: ChangeDetectionStrategy.Eager,
-  template: '<p class="text-white pl-18">{{message}}</p>',
+  template: '<p class="text-white pl-1">{{message}}</p>',
 })
 export class ValidationMessage {
-  @Input() message: string = '';
+  @Input() message: string | undefined = '';
 }
