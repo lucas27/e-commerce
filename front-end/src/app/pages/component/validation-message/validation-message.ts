1cd676894779829acc876640546bb989aff4ef2f
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-19 20:01:54 -0300

    feat: change in error message system

--- front-end/src/app/pages/component/validation-message/validation-message.ts
+++ front-end/src/app/pages/component/validation-message/validation-message.ts
@@ -1,10 +1,21 @@
-import { Component, inject, input, Input, ChangeDetectionStrategy, signal } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, input } from '@angular/core';
+import { ValidationError } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-validation-message',
   changeDetection: ChangeDetectionStrategy.Eager,
-  template: '<p class="text-white pl-1">{{message}}</p>',
+  template: `
+  @if(isTouched && isInvalid()) {
+    @for(errors of error(); track errors) {
+      <p class="text-white pl-1">{{errors.message}}</p>
+    }
+  }
+  `,
 })
 export class ValidationMessage {
-  @Input() message: string | undefined = '';
+  // duas formas de fazer
+  @Input() isTouched: boolean = false;
+
+  readonly isInvalid = input.required<boolean>();
+  readonly error = input.required<ValidationError.WithFieldTree[]>();
 }
