d9f057f5cc1f851692e39407a3b9ff6d6960c157
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-20 20:35:56 -0300

    feat: add register route

--- front-end/src/app/pages/register/register.ts
+++ front-end/src/app/pages/register/register.ts
@@ -1,17 +1,59 @@
-<<<<<<< HEAD
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-=======
-import { Component } from '@angular/core';
->>>>>>> 80db5a919bccb7a906d62d2f8bd811b2b1754c4e
+import { Component, ChangeDetectionStrategy, signal, inject } from '@angular/core';
+import { form, FormField, submit } from '@angular/forms/signals';
+import { LucideCircleUser } from '@lucide/angular';
+import { ValidateRegisterForm } from './service/validate-register-form';
+import { ValidationMessage } from '../component/validation-message/validation-message';
+
+interface validationRegister {
+  name: string;
+  email: string;
+  password: string;
+  againPassword: string;
+}
+
+interface signUp {
+  name: string,
+  email: string,
+  password: string
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormField, LucideCircleUser, ValidationMessage],
   templateUrl: './register.html',
-<<<<<<< HEAD
   changeDetection: ChangeDetectionStrategy.Eager,
-=======
->>>>>>> 80db5a919bccb7a906d62d2f8bd811b2b1754c4e
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  private validation = inject(ValidateRegisterForm);
+
+  private createAccountModel = signal<validationRegister>({
+    name: '',
+    email: '',
+    password: '',
+    againPassword: '' 
+  });
+
+  protected createAccountForm = form(this.createAccountModel, (path) => {
+    // Para diminuir as linhas de código, 
+    // eu cologuei toda lógica de validção dentro do validate-register-form.ts
+    this.validation.validation(path);
+  });
+
+ async onSubmit(event:Event){
+  // onSubmit(){
+    event.preventDefault();
+
+    const success = await submit(this.createAccountForm, async (schema) => {
+      
+      const credentials:signUp = {
+        name: schema.name().value(),
+        email: schema.email().value(),
+        password: schema.password().value() 
+      }
+      console.log(credentials);
+      
+    });
+    console.log(success)
+  }
+}
