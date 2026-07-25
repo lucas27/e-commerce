35de001d3e015ce2641d680738250e9ffb72d397
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-25 20:32:35 -0300

    feat: add lazy-loaded component

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
 import { Cart } from './pages/cart/cart';
 
 export const routes: Routes = [
-    { path: '', component: Home, title: "main page" },
+    { path: '', loadComponent: () => import('./pages/home/home').then(component => component.Home), title: "main page" },
     { path: 'register', component: Register, title:"register page" },
     { path: 'login', component: Login, title: "login page" },
     { path: 'cart', component: Cart, title: "cart page" }
