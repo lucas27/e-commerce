309fe265c365b7d1d4f4d2399255468b7f39d141
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-24 21:03:34 -0300

    remove: user role

--- back-end/src/main/java/com/projeto/e_commerce/auth/dto/RegisterDto.java
+++ back-end/src/main/java/com/projeto/e_commerce/auth/dto/RegisterDto.java
@@ -1,10 +1,7 @@
 package com.projeto.e_commerce.auth.dto;
 
-import com.projeto.e_commerce.auth.enums.RoleEnum;
-
 public record RegisterDto(
     String name,
     String email,
-    String password,
-    RoleEnum role
+    String password
 ) {}
