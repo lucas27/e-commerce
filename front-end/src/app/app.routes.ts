99020a4821011c6a037f6cb71b34a0ed79b0064b
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-28 18:24:46 -0300

    feat: create guard route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
 import { Cart } from './pages/cart/cart';
+import { Home } from './pages/home/home';
+Home
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./pages/home/home').then(component => component.Home), title: "main page" },
     { path: 'register', component: Register, title:"register page" },
     { path: 'login', component: Login, title: "login page" },
-    { path: 'cart', component: Cart, title: "cart page" }
+    { path: 'cart', component: Cart, title: "cart page" },
+    // { path: 'create', title: "create itens"},
+    { path: '**', redirectTo: ''}
 ];
