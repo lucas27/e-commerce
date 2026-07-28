45b5748239835e86f0eada3555804925dc0fb6c7
lucas27 <lucasbrasileiro27@gmail.com>
2026-07-28 18:25:48 -0300

    remove: rxjs

--- front-end/src/app/pages/home/component/header/component/account/service/accountService.ts
+++
@@ -1,7 +0,0 @@
-import { Service } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-
-@Service()
-export class AccountService {
-    public needCall$ = new BehaviorSubject<boolean>(true);
-}
